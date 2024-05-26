21f8f89da07685fbf7e011c13767be431364c04d
Rob van der Most <silvester747@gmail.com>
2024-05-26 19:56:02 +0200

    feat: attach, detach, and swap buffer

--- vimshady/plugin.py
+++ vimshady/plugin.py
@@ -15,19 +15,31 @@ class VimShadyPlugin(object):
 
     @pynvim.command("VimShady")
     def vim_shady(self):
-        """Start a shader window for the current buffer"""
-        self.attached_buffer_number = self.nvim.current.buffer.number
-
-        self.logger = LogWindow(self.nvim)
-        self.render_client = start_render_server()
-        self.update_shader()
+        """Start/stop a shader window for the current buffer"""
+        if self._is_current_buffer_attached():
+            self._detach_from_current_buffer()
+        else:
+            self._attach_to_current_buffer()
 
     @pynvim.autocmd("BufWrite", pattern="*.glsl")
     def buffer_written(self):
-        if self.nvim.current.buffer.number == self.attached_buffer_number:
-            self.update_shader()
+        if self._is_current_buffer_attached():
+            self._update_shader()
 
-    def update_shader(self):
+    def _attach_to_current_buffer(self):
+        self.attached_buffer_number = self.nvim.current.buffer.number
+        self.logger = LogWindow(self.nvim)
+        if self.render_client is None:
+            self.render_client = start_render_server()
+        self.logger.append(f"Rendering shader from `{self.nvim.current.buffer.name}`")
+        self._update_shader()
+
+    def _detach_from_current_buffer(self):
+        self.attached_buffer_number = None
+        self.logger.close()
+        self.render_client = None
+
+    def _update_shader(self):
         try:
             fragment_source = "\n".join(self.nvim.current.buffer)
             result = self.render_client.update_shader_source(fragment_source)
@@ -39,6 +51,9 @@ class VimShadyPlugin(object):
         except Exception as ex:
             self.logger.append(*str(ex).split("\n"))
 
+    def _is_current_buffer_attached(self):
+        return self.nvim.current.buffer.number == self.attached_buffer_number
+
 
 class LogWindow(object):
     def __init__(self, nvim):
@@ -47,12 +62,12 @@ class LogWindow(object):
         self.buffer = None
         self.window = None
 
-        self.ensure_buffer()
-        self.ensure_window()
+        self._ensure_buffer()
+        self._ensure_window()
 
     def append(self, *text):
-        self.ensure_buffer()
-        self.ensure_window()
+        self._ensure_buffer()
+        self._ensure_window()
 
         self.buffer.api.set_option("modifiable", True)
         self.buffer.append(text)
@@ -60,7 +75,17 @@ class LogWindow(object):
         self.buffer.api.set_option("modified", False)
         self.window.api.set_cursor((len(self.buffer), 0))
 
-    def ensure_buffer(self):
+    def close(self):
+        if self.window is not None:
+            if self.window.valid:
+                self.window.api.close(True)
+            self.window = None
+        if self.buffer is not None:
+            if self.buffer.valid:
+                self.buffer.api.delete({"force": True})
+            self.buffer = None
+
+    def _ensure_buffer(self):
         if self.buffer is None or not self.buffer.valid:
             self.buffer = self.nvim.api.create_buf(False, True)
             if not self.buffer.valid:
@@ -70,7 +95,7 @@ class LogWindow(object):
 
             self.window = None
 
-    def ensure_window(self):
+    def _ensure_window(self):
         if self.window is None or not self.window.valid or self.window.buffer.number != self.buffer.number:
             self.window = self.nvim.api.open_win(
                 self.buffer,
@@ -83,3 +108,4 @@ class LogWindow(object):
             )
             if not self.window.valid:
                 raise Exception("Could not create log window")
+
